d5d08ba14444d3016df1a3e3bd39ab9df9ae7161
FreeSoulCris <c.jimenez@witbooking.com>
2023-07-14 18:42:28 +0200

    Multiple locations TimeZones JS

--- javascript/index.js
+++ javascript/index.js
@@ -4,43 +4,67 @@ function updateTime() {
 
   let losAngelesElement = document.querySelector("#los-angeles");
 
-  let losAngelesDateElement = losAngelesElement.querySelector(".date");
+  if (losAngelesElement) {
+    let losAngelesDateElement = losAngelesElement.querySelector(".date");
 
-  let losAngelesTimeElement = losAngelesElement.querySelector(".time");
+    let losAngelesTimeElement = losAngelesElement.querySelector(".time");
 
-  //llamada a la api de moment js para la info de fecha, hora y timezone
-  let losAngelesCurrentTime = moment().tz("America/Los_Angeles");
+    //llamada a la api de moment js para la info de fecha, hora y timezone
+    let losAngelesCurrentTime = moment().tz("America/Los_Angeles");
 
-  //Tras vincular moment.js en index.html, cambio los elementos date y time para que aparezcan datos reales que provienen de la API de moment.js
-  losAngelesDateElement.innerHTML = moment().format("MMMM Do YYYY");
+    //Tras vincular moment.js en index.html, cambio los elementos date y time para que aparezcan datos reales que provienen de la API de moment.js
+    losAngelesDateElement.innerHTML = moment().format("MMMM Do YYYY");
 
-  // Opcion 1: con interporlación
-  losAngelesTimeElement.innerHTML = `${losAngelesCurrentTime.format(
-    "H:mm:ss "
-  )}<small>${losAngelesCurrentTime.format("A")}</small>`;
-
+    // Opcion 1: con interporlación
+    losAngelesTimeElement.innerHTML = `${losAngelesCurrentTime.format(
+      "H:mm:ss "
+    )}<small>${losAngelesCurrentTime.format("A")}</small>`;
+  }
   //OPCION 2 para actualizar el tiempo cada segundo:
 
   // BARCELONA
   //Selecciono en el doc HTML el elemento con id los Angeles para reemplazar los datos utilizando innerHTML. Clases date y time.
 
   let barcelonaElement = document.querySelector("#barcelona");
+  if (barcelonaElement) {
+    let barcelonaDateElement = barcelonaElement.querySelector(".date");
 
-  let barcelonaDateElement = barcelonaElement.querySelector(".date");
+    let barcelonaTimeElement = barcelonaElement.querySelector(".time");
 
-  let barcelonaTimeElement = barcelonaElement.querySelector(".time");
+    let barcelonaCurrentTime = moment().tz("Europe/Madrid");
 
-  let barcelonaCurrentTime = moment().tz("Europe/Madrid");
+    //Tras vincular moment.js en index.html, cambio los elementos date y time para que aparezcan datos reales que provienen de la API de moment.js
+    barcelonaDateElement.innerHTML = moment().format("MMMM Do YYYY");
 
-  //Tras vincular moment.js en index.html, cambio los elementos date y time para que aparezcan datos reales que provienen de la API de moment.js
-  barcelonaDateElement.innerHTML = moment().format("MMMM Do YYYY");
+    barcelonaTimeElement.innerHTML = moment().format("H:mm:ss A");
 
-  barcelonaTimeElement.innerHTML = moment().format("H:mm:ss A");
-
-  //Opción 2: más simple y limpia que con interpolación
-  barcelonaTimeElement.innerHTML = barcelonaCurrentTime.format(
-    "H:mm:ss [<small>]A[</small>]"
-  );
+    //Opción 2: más simple y limpia que con interpolación
+    barcelonaTimeElement.innerHTML = barcelonaCurrentTime.format(
+      "H:mm:ss [<small>]A[</small>]"
+    );
+  }
 }
+
+function updateCity(event) {
+  let cityTimeZone = event.target.value;
+  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+  let cityTime = moment().tz(cityTimeZone);
+  let citiesElement = document.querySelector("#cities");
+  citiesElement.innerHTML = `<div class="city">
+  <div>
+  <h2>${cityName}</h2>
+  <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
+  </div>
+  <div class="time">${cityTime.format("h:mm:ss")}   <small>${cityTime.format(
+    "A"
+  )}</small></div>
+  </div>
+  `;
+}
+
 updateTime();
 setInterval(updateTime, 1000);
+
+let citiesSelectElement = document.querySelector("#city-selector");
+
+citiesSelectElement.addEventListener("change", updateCity);
